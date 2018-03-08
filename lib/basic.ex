b353601ce36ca59243b42b6d05e5be929e2585eb
Ed <eudes.scar@gmail.com>
2018-03-07 23:43:21 -0300

    Reorganize modules - `Moderation` module - Removed `Fun` module and moved the functions to `Basic` - `Utils` module with some "non-cog" functions - New commands: `set_prefix`, `ohno`, `roll`, `userinfo`

--- lib/basic.ex
+++ lib/basic.ex
@@ -1,6 +1,23 @@
 defmodule Thonk.Basic do
   use Alchemy.Cogs
+  alias Thonk.Utils
+  alias Alchemy.{Client, Voice}
+  require Alchemy.Embed, as: Embed
 
+  @yellow 0xfac84b
+
+  Cogs.def help do
+    commands = Cogs.all_commands()
+    |> Map.keys()
+    |> Enum.join("\n")
+
+    %Embed{color: @yellow, title: "All available commands", description: commands}
+    |> Embed.send()
+  end
+
+  @doc """
+  Roll dice.
+  """
   Cogs.set_parser(:roll, &List.wrap/1)
   Cogs.def roll(times) do
     times =
@@ -18,24 +35,76 @@ defmodule Thonk.Basic do
         |> Enum.join(", ")
 
         ":game_die: You rolled **#{times}** times!\n**#{numbers}**"
-        |> exceed_check()
+        |> Utils.exceed_check()
         |> Cogs.say()
     end
   end
 
-  defp exceed_check(input) do
-    size = String.length(input)
+  @doc """
+  oh no
+  """
+  Cogs.def ohno do
+    Client.send_message(message.channel_id, "oh no", file: "lib/assets/ohno.png")
+  end
 
-    cond do
-      size > 2000 ->
-        res = HTTPoison.post!("https://hastebin.com/documents", input)
-        link = res.body
-        |> Poison.decode!()
-        |> Map.get("key")
+  @moduledoc """
+  Plays a gemidao in a voice channel.
+  """
+  Cogs.def gemidao do
+    {:ok, guild} = Cogs.guild()
+    voice_channel = Enum.find(guild.channels, &match?(%{type: :voice}, &1))
 
-        ":warning: The command output exceeded the characters limit! (`#{size}/2000`)\nYou can check it out here: https://hastebin.com/#{link}"
-      true ->
-        input
+    Voice.join(guild.id, voice_channel.id)
+    Voice.play_file(guild.id, "lib/assets/gemidao.mp3")
+  end
+
+  @doc """
+  Gets a random comment from brazilian porn on xvideos.
+
+  Inspired by `https://github.com/ihavenonickname/bot-telegram-comentarios-xvideos`.
+  """
+  Cogs.def xvideos do
+    {title, %{"c" => content, "n" => author}} = get_comment()
+    title   = Utils.escape(title)
+    author  = Utils.escape(author)
+    content = Utils.escape(content)
+
+    %Embed{color: 0xe80000, title: "XVideos"}
+    |> Embed.field("Título:", "**`#{title}`**")
+    |> Embed.field("#{author} comentou:", "**`#{content}`**")
+    |> Embed.send()
+  end
+
+  @spec fetch_page(number) :: {String.t, String.t}
+  defp fetch_page(page_number) do
+    res = HTTPoison.get!("https://www.xvideos.com/porn/portugues/#{page_number}")
+    res.body
+    |> Floki.parse()
+    |> Floki.find(".thumb-block > p > a")
+    |> Enum.map(fn {_tag, info, _title} ->
+      [{_, href}, {_, title}] = info
+      [_, video_ref] = Regex.run(~r{/video(\d+)/.*}, href)
+      {title, video_ref}
+    end)
+  end
+
+  @spec get_comment :: {String.t, map}
+  defp get_comment do
+    videos = fetch_page(Enum.random(1..40))
+    [{title, video_ref}] = Enum.take_random(videos, 1)
+
+    res = HTTPoison.get!("https://www.xvideos.com/video-get-comments/#{video_ref}/0")
+    comment = res.body
+    |> Poison.decode!()
+    |> Map.get("comments")
+    |> Enum.take_random(1)
+
+    # Handle empty comments
+    case comment do
+      [] ->
+        get_comment()
+      [c] ->
+        {title, Map.take(c, ["c", "n"])}
     end
   end
 end
--- lib/utils.ex
+++ lib/utils.ex
@@ -1,58 +1,7 @@
 defmodule Thonk.Utils do
-  use Alchemy.Cogs
-  alias Alchemy.{Cache, Client, Permissions, User}
-  require Alchemy.Embed, as: Embed
+  alias Alchemy.Permissions
 
-  @yellow 0xfac84b
-
-  Cogs.def help do
-    commands = Cogs.all_commands()
-    |> Map.keys()
-    |> Enum.join("\n")
-
-    %Embed{color: @yellow, title: "All available commands", description: commands}
-    |> Embed.send()
-  end
-
-  Cogs.def set_prefix(new_prefix) do
-    {:ok, guild} = Cogs.guild()
-
-    case admin_check(guild, message.author.id) do
-      false ->
-        Cogs.say("You are not an administrator!")
-      true ->
-        Cogs.set_prefix(new_prefix)
-        Application.put_env(:thonk, :prefix, new_prefix)
-        Cogs.say("New prefix: `#{new_prefix}`")
-    end
-  end
-
-  Cogs.def info do
-    {:ok, app_version} = :application.get_key(:thonk, :vsn)
-    {:ok, lib_version} = :application.get_key(:alchemy, :vsn)
-    {:ok, guilds} = Client.get_current_guilds()
-
-    infos = [
-      {"Prefix", Application.get_env(:thonk, :prefix)},
-      {"Version", "#{app_version}"},
-      {"Elixir Version", System.version()},
-      {"Library", "[Alchemy #{lib_version}](https://github.com/cronokirby/alchemy)"},
-      {"Owner", "[appositum#7545](https://github.com/appositum)"},
-      {"Guilds", "#{length(guilds)}"},
-      {"Processes", "#{length :erlang.processes()}"},
-      {"Memory Usage", "#{div :erlang.memory(:total), 1_000_000} MB"}
-    ]
-
-    Enum.reduce(infos, %Embed{color: @yellow, title: "Thonk"}, fn {name, value}, embed ->
-      Embed.field(embed, name, value, inline: true)
-    end)
-    |> Embed.thumbnail("http://i.imgur.com/6YToyEF.png")
-    |> Embed.url("https://github.com/appositum/thonk")
-    |> Embed.footer(text: "Uptime: #{uptime()}")
-    |> Embed.send()
-  end
-
-  defp uptime do
+  def uptime do
     {time, _} = :erlang.statistics(:wall_clock)
     min = div(time, 1000 * 60)
     {hours, min} = {div(min, 60), rem(min, 60)}
@@ -66,7 +15,7 @@ defmodule Thonk.Utils do
   end
 
   @spec admin_check(%Alchemy.Guild{}, String.t) :: boolean
-  defp admin_check(guild, user_id) do
+  def admin_check(guild, user_id) do
     member = guild.members
     |> Enum.find(&(&1.user.id == user_id))
 
@@ -74,9 +23,35 @@ defmodule Thonk.Utils do
     |> Enum.filter(fn role -> role.id in member.roles end)
     |> Enum.map(fn role ->
       role.permissions
-      |> Permissions.contains?(role, :administrator)
+      |> Permissions.contains?(:administrator)
     end)
 
     true in roles
+  end
+
+  @doc """
+  Check if some string exceeds discod's characters limit (2000).
+  """
+  def exceed_check(input) do
+    size = String.length(input)
+
+    cond do
+      size > 2000 ->
+        res = HTTPoison.post!("https://hastebin.com/documents", input)
+        link = res.body
+        |> Poison.decode!()
+        |> Map.get("key")
+
+        ":warning: The command output exceeded the characters limit! (`#{size}/2000`)\nYou can check it out here: https://hastebin.com/#{link}"
+      true ->
+        input
+    end
+  end
+
+  @spec escape(String.t) :: String.t
+  def escape(string) do
+    Regex.replace(~r{</?(a|A).*?>}, string, "")
+    |> HtmlEntities.decode()
+    |> String.replace("<br />", "\n")
   end
 end
