4cae35ce408a8385b176da49537dc7f15f56be1c
appositum <eudes.scar@gmail.com>
2018-03-17 00:04:22 -0300

    Some useless stupid tests

--- test/thonk_test.exs
+++ test/thonk_test.exs
@@ -1,8 +1,22 @@
 defmodule ThonkTest do
   use ExUnit.Case
+  alias Thonk.Utils
   doctest Thonk
 
-  test "greets the world" do
-    assert Thonk.hello() == :world
+  test "exceeded characters limit" do
+    assert Utils.check_exceed("testing") == "testing"
+    assert Stream.cycle(["a"])
+    |> Enum.take(4000)
+    |> Enum.join()
+    |> Utils.check_exceed()
+    |> String.starts_with?(":warning: The command output exceeded the characters limit! (`4000/2000`)")
+  end
+
+  test "random hex" do
+    pattern = ~r/^([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/
+
+    assert Regex.match?(pattern, Utils.color_random())
+    assert Regex.match?(pattern, "Ff00a3")
+    assert Regex.match?(pattern, "FF00AZ") == false
   end
 end
