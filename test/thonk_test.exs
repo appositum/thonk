14b3ea733dfd313ab7b4c382ff45ee426ce2fd9d
appositum <eudes.scar@gmail.com>
2018-04-02 19:22:23 -0300

    Update tests

--- test/thonk_test.exs
+++ test/thonk_test.exs
@@ -3,18 +3,28 @@ defmodule ThonkTest do
   alias Thonk.Utils
   doctest Thonk
 
-  test "exceeded characters limit" do
-    assert Utils.check_exceed("testing") == "testing"
+  test "exceeded characters limit hastebin" do
+    assert Utils.message_exceed("testing") == "testing"
     assert Stream.cycle(["a"])
     |> Enum.take(4000)
     |> Enum.join()
-    |> Utils.check_exceed()
-    |> String.starts_with?(":warning: The command output exceeded the characters limit! (`4000/2000`)")
+    |> Utils.message_exceed()
+    |> String.starts_with?(":warning: **The command output exceeded the characters limit! (`4000/2000`)")
+  end
+
+  test "exceeded characters limit boolean" do
+    assert Utils.message_exceed?("test") == false
+    assert Stream.cycle(["a"])
+    |> Enum.take(4000)
+    |> Enum.join()
+    |> Utils.message_exceed?()
   end
 
   test "random hex" do
     pattern = ~r/^#?([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/
 
+    assert Regex.match?(pattern, Utils.color_random())
+    assert Regex.match?(pattern, Utils.color_random())
     assert Regex.match?(pattern, Utils.color_random())
     assert Regex.match?(pattern, "Ff00a3")
     assert Regex.match?(pattern, "FF20Fg") == false
