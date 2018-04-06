0410057327e3266bb948a92270da458c7f75d3b1
appositum <eudes.scar@gmail.com>
2018-04-06 02:11:16 -0300

    Remove unused dependencies and tests

--- test/thonk_test.exs
+++ test/thonk_test.exs
@@ -19,17 +19,4 @@ defmodule ThonkTest do
     |> Enum.join()
     |> Utils.message_exceed?()
   end
-
-  test "random hex" do
-    pattern = ~r/^#?([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/
-
-    assert Regex.match?(pattern, Utils.color_random())
-    assert Regex.match?(pattern, Utils.color_random())
-    assert Regex.match?(pattern, Utils.color_random())
-    assert Regex.match?(pattern, "Ff00a3")
-    assert Regex.match?(pattern, "FF20Fg") == false
-    assert Regex.match?(pattern, "FF00AZ") == false
-    assert Regex.match?(pattern, "#FF00AZ") == false
-    assert Regex.match?(pattern, "#E80000")
-  end
 end
